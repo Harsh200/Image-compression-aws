9e2729a59bbda49b9b3bfd302d7e086a4021ee84
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2020-02-09 17:54:01 +0530

    defining function for url

--- handler.py
+++ handler.py
@@ -6,3 +6,8 @@ import boto3
 # Using python image library for image compression
 import PIL
 from PIL import Image
+
+def resized_image_url(resized_key, bucket, region):
+    return "https://s3-{region}.amazonaws.com/{bucket}/{resized_key}".format(
+        bucket=bucket, region=region, resized_key=resized_key
+    )
